5a56b467a8e6f44efb59bd1971d512e4de497277
Miczu <48890028+Miczu@users.noreply.github.com>
2020-05-23 14:22:06 +0200

    Script for setting global variable

---
+++ twitch_fragments/outcomes/ukko_in_a_box.lua
@@ -0,0 +1,11 @@
+--Ukko in a box
+--What are the odds of that?
+--bad
+--10
+--todo
+function twitch_ukko_in_a_box()
+	local countString = GlobalsGetValue("twitch_ukko_in_box", "0")
+	local countNumber = tonumber(countString) + 1
+	local newN =  tostring(countNumber)
+	GlobalsSetValue("twitch_ukko_in_box", newN)
+end
