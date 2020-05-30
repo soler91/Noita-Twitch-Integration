92477a1af236f3fc2447cf1cca953aa16365cddb
Soler91 <boskozarich@gmail.com>
2020-05-30 09:39:51 -0400

    make less boring

--- twitch_fragments/outcomes/lava_sea.lua
+++ twitch_fragments/outcomes/lava_sea.lua
@@ -1,9 +1,19 @@
---Sea of Lava
+--Lava pit
 --Watch your step
 --enviromental
 --90
 --todo
 function twitch_lava_sea()
-    spawn_item_in_range("data/entities/projectiles/deck/sea_lava.xml", 0, 200,
-                        20, 100, 0, 1, false)
+    local x, y = get_player_pos()
+	local digger = EntityCreateNew()
+	EntityAddComponent(digger, "CellEaterComponent", {
+		radius = "164",
+		eat_probability = "1000000000",
+		eat_dynamic_physics_bodies = "1"
+	})
+	EntityAddComponent(digger, "LifetimeComponent", {
+		lifetime = "1"
+	})
+	EntitySetTransform(digger, x, y + 90)
+	EntityLoad("data/entities/projectiles/deck/sea_lava.xml", x, y + 50)
 end
