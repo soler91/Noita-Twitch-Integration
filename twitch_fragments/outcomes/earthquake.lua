a8197d1bda8565474511e1e46e89977298c66ede
Miczu <48890028+Miczu@users.noreply.github.com>
2020-07-31 22:57:12 +0200

    +0.75s for running

--- twitch_fragments/outcomes/earthquake.lua
+++ twitch_fragments/outcomes/earthquake.lua
@@ -4,7 +4,12 @@
 --20
 --todo
 function twitch_earthquake()
-	local x, y = get_player_pos()
-	local hit, hx, hy = RaytracePlatforms(x, y, x, y - 500)
-	EntityLoad("data/entities/projectiles/deck/crumbling_earth.xml", hx, (y+hy)/2)
+	async(function()
+		local x, y = get_player_pos()
+		local hit, hx, hy = RaytracePlatforms(x, y, x, y - 500)
+		local yy = (y+hy)/2;
+		EntityLoad("data/entities/particles/image_emitters/magical_symbol.xml", hx, yy)
+		wait(45)
+		EntityLoad("data/entities/projectiles/deck/crumbling_earth.xml", hx, yy)
+	end)
 end
