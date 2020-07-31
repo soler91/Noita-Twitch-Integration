c40607b122ea0bdb0d0e4a621fa98cad082b92c7
Soler91 <boskozarich@gmail.com>
2020-07-31 03:14:18 -0400

    no boss name

--- twitch_fragments/utils.lua
+++ twitch_fragments/utils.lua
@@ -8,14 +8,18 @@ async_loop(function()
 
     if (#twitch_viewers > 0 and GameGetFrameNum() % 300 == 0) then
         local x, y = get_player_pos()
-        for _, entity in pairs(EntityGetInRadiusWithTag(x, y, 4096, "enemy") or {}) do
-            if (EntityHasTag(entity, "dont_append_name") == false) then
-                
-                local r = math.random(100)
-                if (r > 60) then
-                    append_viewer_name(entity)
-                else
-                    EntityAddTag(entity, "dont_append_name")
+        for _, entity in pairs(EntityGetInRadiusWithTag(x, y, 4096, "enemy") or
+                                   {}) do
+            if (EntityHasTag(entity, "boss_centipede") == false or
+                EntityHasTag(entity, "boss_centipede_active") == true) then
+                if (EntityHasTag(entity, "dont_append_name") == false) then
+
+                    local r = math.random(100)
+                    if (r > 60) then
+                        append_viewer_name(entity)
+                    else
+                        EntityAddTag(entity, "dont_append_name")
+                    end
                 end
             end
         end
