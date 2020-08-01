be5408c1988268de2ae9d0752b629134c434b236
Soler91 <boskozarich@gmail.com>
2020-08-01 04:35:17 -0400

    lava pit revamp

--- twitch_fragments/outcomes/lava_sea.lua
+++ twitch_fragments/outcomes/lava_sea.lua
@@ -5,15 +5,5 @@
 --todo
 function twitch_lava_sea()
     local x, y = get_player_pos()
-	local digger = EntityCreateNew()
-	EntityAddComponent(digger, "CellEaterComponent", {
-		radius = "164",
-		eat_probability = "1000000000",
-		eat_dynamic_physics_bodies = "1"
-	})
-	EntityAddComponent(digger, "LifetimeComponent", {
-		lifetime = "1"
-	})
-	EntitySetTransform(digger, x, y + 90)
-	EntityLoad("data/entities/projectiles/deck/sea_lava.xml", x, y + 50)
+	LoadPixelScene("mods/twitch-integration/files/pixel_scenes/lava_pit.png","mods/twitch-integration/files/pixel_scenes/lava_pit_visuals.png", x - 250, y, "", true)
 end
