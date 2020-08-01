5f63d176059c567b5e8548d9c79a4ae69419da79
Miczu <48890028+Miczu@users.noreply.github.com>
2020-08-01 20:14:16 +0200

    ultimate & names

--- twitch_fragments/outcomes/angry_steve.lua
+++ twitch_fragments/outcomes/angry_steve.lua
@@ -4,5 +4,5 @@
 --80
 --todo
 function twitch_angry_steve()
-	spawn_entity_in_view_random_angle("data/entities/animals/necromancer_shop.xml", 75, 160)
+	spawn_entity_in_view_random_angle("data/entities/animals/necromancer_shop.xml", 75, 160, 20, append_viewer_name)
 end
