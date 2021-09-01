35b60015fc8cfb99c54388adb1f2124b8fd3afef
Soler91 <boskozarich@gmail.com>
2021-09-01 04:55:34 -0400

    fix shifting

--- twitch_fragments/outcomes/shift.lua
+++ twitch_fragments/outcomes/shift.lua
@@ -6,6 +6,11 @@
 function twitch_shift()
     local player = get_player()
     local fungi = CellFactory_GetType("fungi")
-    EntityIngestMaterial( player, fungi, 300 )
+    local frame = GameGetFrameNum()
+	local last_frame = tonumber( GlobalsGetValue( "fungal_shift_last_frame", "-1000000" ) )
+	if frame < last_frame + 60*60*5 and not debug_no_limits then
+		return -- long cooldown
+	end
+    EntityIngestMaterial( player, fungi, 600 )
     empty_player_stomach()
 end
