6cdf88d24200e91b3f87a6058beb876a0f49da9e
Soler91 <boskozarich@gmail.com>
2022-03-19 06:23:11 -0400

    actually random

--- twitch_fragments/outcomes/random_teleport.lua
+++ twitch_fragments/outcomes/random_teleport.lua
@@ -5,6 +5,6 @@
 --todo
 function twitch_random_teleport()
     local player = get_player()
-    local game_effect = GetGameEffectLoadTo(player, "TELEPORTATION", true);
+    local game_effect = GetGameEffectLoadTo(player, "UNSTABLE_TELEPORTATION", true);
     if game_effect ~= nil then ComponentSetValue(game_effect, "frames", 60); end
 end
