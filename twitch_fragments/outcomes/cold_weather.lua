a62ce1a817b0a7a659d07158caef513bb6312e79
Miczu <48890028+Miczu@users.noreply.github.com>
2022-10-29 13:15:10 +0200

    Fixed tabs

--- twitch_fragments/outcomes/cold_weather.lua
+++ twitch_fragments/outcomes/cold_weather.lua
@@ -8,14 +8,14 @@ function twitch_cold_weather()
         while true do
             wait(6)
             player_data = EntityGetFirstComponent( get_player(), "CharacterDataComponent")
-			if (player_data ~= nil) then				
-				local flytime = ComponentGetValue2(player_data, "mFlyingTimeLeft")
-				local flyMax = ComponentGetValue2(player_data, "fly_time_max")
-				if (flytime == flyMax) then
-					spawn_item_in_range("data/entities/projectiles/deck/sea_ice.xml", 0, 0, 0, 15, 0, 1, false)
-					return;
-				end
-			end			
+            if (player_data ~= nil) then                
+                local flytime = ComponentGetValue2(player_data, "mFlyingTimeLeft")
+                local flyMax = ComponentGetValue2(player_data, "fly_time_max")
+                if (flytime == flyMax) then
+                    spawn_item_in_range("data/entities/projectiles/deck/sea_ice.xml", 0, 0, 0, 15, 0, 1, false)
+                    return;
+                end
+            end            
         end
     end)
 end
