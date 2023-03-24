678b593eca1f22b798f153b29fa300b1f67b7797
Conga Lyne <43751663+Conga0@users.noreply.github.com>
2023-03-24 16:27:29 +0930

    Balance Patch #2
    Poisoned Water no longer does stain damage, you're allowed to extinguish yourself now
    Poisoned Water does 70% less damage on material contact
    Homing Boomerang's rubberband effect is now 33% slower, giving you more time to teleport away before it catches back up
    Fixed Possess wands not properly playing an audio que
    Fixed Homing Blackhole not having a speed reduction (Hey, you were dodging it pre-nerf! good   job god gamer old man!)

--- files/scripts/effect_blackhole_boomerang_rubberband.lua
+++ files/scripts/effect_blackhole_boomerang_rubberband.lua
@@ -4,7 +4,7 @@ local pos_x, pos_y = EntityGetTransform(entity_id)
 local comp = EntityGetFirstComponentIncludingDisabled(entity_id,"GhostComponent")
 
 if #EntityGetInRadiusWithTag(pos_x, pos_y, 200, "player_unit") <= 0 then
-    ComponentSetValue2(comp,"speed",90)
+    ComponentSetValue2(comp,"speed",60)
 else
-    ComponentSetValue2(comp,"speed",40)
+    ComponentSetValue2(comp,"speed",30)
 end
--- files/scripts/hex_water_persistent.lua
+++ files/scripts/hex_water_persistent.lua
@@ -3,5 +3,5 @@ entity_id = EntityGetRootEntity(entity_id)
 
 local effectTest = GameGetGameEffectCount( entity_id, "WET" )
 if effectTest >= 1 then
-     EntityInflictDamage( entity_id, 0.020, "DAMAGE_CURSE", "Abyssal Hex", "NONE", 0, 0, 0 )
+     EntityInflictDamage( entity_id, 0.004, "DAMAGE_CURSE", "Abyssal Hex", "NONE", 0, 0, 0 )
 end
