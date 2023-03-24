5a93a2e0124b22a5f80f121566ee5c6c58863414
Conga Lyne <43751663+Conga0@users.noreply.github.com>
2023-03-25 01:16:36 +0930

    Monstrous Drink fix
    Further reduced Homing Blackhole Speed by 10% (30 to 27)
    Fixed Monstrous Drink not properly adding monstrous powder to the player's potion (Thankyou Miczu for pointing this out!)

--- files/scripts/effect_blackhole_boomerang_rubberband.lua
+++ files/scripts/effect_blackhole_boomerang_rubberband.lua
@@ -6,5 +6,5 @@ local comp = EntityGetFirstComponentIncludingDisabled(entity_id,"GhostComponent"
 if #EntityGetInRadiusWithTag(pos_x, pos_y, 200, "player_unit") <= 0 then
     ComponentSetValue2(comp,"speed",60)
 else
-    ComponentSetValue2(comp,"speed",30)
+    ComponentSetValue2(comp,"speed",27)
 end
